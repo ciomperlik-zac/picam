cd2e2e300858e1f6c37d90a2d74d802fcc3b7f49
ciomperlik-zac <148258873+ciomperlik-zac@users.noreply.github.com>
2024-12-28 10:43:14 -0600

    Add files via upload

---
+++ endpoint.py
@@ -0,0 +1,30 @@
+import socket
+import os
+
+ip = socket.gethostbyname(socket.gethostname())
+
+print(f"Pi Cam Endpoint\n\nIP: {ip}\n")
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((ip, 11000))
+server.listen()
+
+def callback(msg):
+    os.system("rundll32.exe user32.dll,LockWorkStation")
+
+while True:
+    client, addr = server.accept()
+    print("> Client connected")
+
+    while True:
+        msg = client.recv(1024).decode("utf-8")
+        
+        if not msg:
+            break
+        else:
+            print(">", msg)
+
+            callback(msg)
+
+    print("> Client disconnected")
+    client.close()
